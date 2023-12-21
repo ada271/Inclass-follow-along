055685805efc55d54114ba5120cee32a3aab8d74
ada <h.kaansolmaz@gmail.com>
2023-12-21 11:42:37 +0300

    folder structure fix

---
+++ javascript/js01/data-types.js
@@ -0,0 +1,16 @@
+/* ---------------------------------- */
+/*              DATATYPES             */
+/* ---------------------------------- */
+
+sayi = 45
+
+console.log(typeof sayi)
+
+/* ---------------------------------- */
+/*        PRIMITIVE DATA TYPES        */
+/* ---------------------------------- */
+
+// tek bir deger tutar
+// number,string, null, undefined, boolean
+
+// BigInt data type cok buyuk sayilar icin kullanilir
