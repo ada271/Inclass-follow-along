fb9d0946c3f86afaefee3cf39c3f1af1fe977e0c
ada <h.kaansolmaz@gmail.com>
2023-12-21 12:00:46 +0300

    br1

--- javascript/js01/data-types.js
+++ javascript/js01/data-types.js
@@ -14,3 +14,19 @@ console.log(typeof sayi)
 // number,string, null, undefined, boolean
 
 // BigInt data type cok buyuk sayilar icin kullanilir
+// number -+ 2 uzeri 53'e kadar tanimlidir
+
+let x = 34
+let y = 89.34235
+
+console.log(typeof x)
+console.log(typeof y)
+
+// math methodu ile matematiksel islemler tanimlanabilir
+
+const pi=Math.PI
+console.log(pi)
+
+let r = 6
+let cevre = 2 * pi * r
+console.log(cevre)
