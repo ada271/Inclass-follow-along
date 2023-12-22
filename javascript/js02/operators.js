30323af802b88d02533531076656bfa73e034f3a
ada <h.kaansolmaz@gmail.com>
2023-12-22 13:15:43 +0300

    br2

---
+++ javascript/js02/operators.js
@@ -0,0 +1,17 @@
+/* ---------------------------------- */
+/*              OPERATORS             */
+/* ---------------------------------- */
+
+let a = 10;
+let b = 2;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a ** b);
+
+// MODULUS
+
+let c = 3;
+console.log(a % c);
