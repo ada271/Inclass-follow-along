8b0eee356d20a1096f773cc15eed9153b9b27b00
ada <h.kaansolmaz@gmail.com>
2023-12-22 14:00:24 +0300

    br2

--- javascript/js02/operators.js
+++ javascript/js02/operators.js
@@ -15,3 +15,37 @@ console.log(a ** b);
 
 let c = 3;
 console.log(a % c);
+
+// EXAMPLE
+
+// let d = +prompt("enter a number");
+
+// if (d % 2 === 0) {
+//     console.log("your number is even");
+// } else {
+//     console.log("your number is odd");
+// }
+
+// EXAMPLE2
+
+let sure = 225;
+
+let saat = parseInt(sure / 60);
+
+console.log(saat)
+
+let dakika = sure % 60
+
+console.log(`${sure} dakika ${saat} ${dakika} dakikadir`)
+
+
+// increment decrement
+
+
+let d = 3
+
+// pre increment
+console.log(++d) 
+// post increment
+console.log(d++) 
+console.log(d)
