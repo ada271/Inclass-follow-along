75103c6cbfdcc08928afd808d792183accc99f42
ada <h.kaansolmaz@gmail.com>
2023-12-22 12:01:25 +0300

    br1

---
+++ javascript/js02/data-conv.js
@@ -0,0 +1,61 @@
+console.log("hello world");
+
+// STRING
+let str1 = "haii world";
+let str2 = "hello world";
+let str3 = `merhaba ${3 + 5}`;
+
+let str4 = new String("Merhaba dunya");
+
+console.log(str3);
+console.log(str2);
+console.log(str1);
+console.log(str4);
+
+console.log(str1[0]);
+
+// NUMBER
+let num1 = 1;
+let num2 = 1.2;
+let num3 = 0.12354124;
+
+console.log(typeof num1);
+console.log(typeof num2);
+console.log(typeof num3);
+
+// BOOLEAN
+
+// true false
+
+console.log(2 < 3);
+console.log(3 < 2);
+
+console.log(typeof true);
+console.log(typeof false);
+
+// FALSY DEGERLER
+// 0 , '', null , undefined
+
+
+
+console.log(Boolean(0));
+console.log(Boolean(null));
+console.log(Boolean(""));
+console.log(Boolean(undefined));
+
+// NULL & undefined & NaN
+
+console.log(typeof null)
+
+let n = null
+console.log(Boolean(n))
+
+console.log(null + 5)
+
+let u;
+
+console.log(Boolean(u))
+console.log(u - 5)
+
+console.log(null == undefined)
+
