f2005bdc44e19bd63060fcc1dd5732c4e1cf426c
ada <h.kaansolmaz@gmail.com>
2024-01-03 15:51:23 +0300

    ex1

--- javascript/js03/conditions.js
+++ javascript/js03/conditions.js
@@ -7,3 +7,28 @@ if ((10 * maas) >= (5000 + borc)) {
 } else{
     console.log('kredi cikmadi');
 }
+
+const num1 = +prompt('enter your first number')
+const operator = prompt('enter an operator')
+const num2 = +prompt('enter your second number')
+
+let result
+
+switch (operator) {
+    case '+':
+            result = num1 + num2
+        break;
+    case '-':
+            result = num1 - num2
+        break;
+    case '*':
+            result = num1 * num2
+        break;
+    case '/':
+            result = num1 / num2
+        break;
+
+}
+
+
+console.log('result is' + result)
